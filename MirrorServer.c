7a148ba9abb09734765a6cf97a95459af41d33fb
Ilias Mentzelos <sdi1400106@di.uoa.gr>
2017-05-24 22:36:38 +0300

    minor updates to Server

--- MirrorServer.c
+++ MirrorServer.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 
 void read_args(int argc, char * argv[], int * port, char ** dirname, int * threadnum);
+char buffer[1024];
+pthread_mutex_t mutex;
+int consumer =0;
+int producer =0;
+pthread_cond_t consumers;
+pthread_cond_t producer;
 
 int main(int argc, char * argv[])
 {
   int port=-1;
   char * dirname=NULL;
-  int threadnum =1;
+  int threadnum =-1;
+
   read_args(argc, argv, &port, &dirname, &threadnum);
+
+  pthread_t worker[threadnum];
+  int i;
+  for (i = 0 ; i < threadnum;  i++)
+  {
+    pthread_create(&worker[i], 0, work, (void *)i);
+  }
   printf("%s \n", dirname );
   free(dirname);
   exit(EXIT_SUCCESS);
@@ -41,4 +56,29 @@ void read_args(int argc, char * argv[], int * port, char ** dirname, int * threa
     printf("Usage: ./MirrorServer -p <port> -m <dirname> -w <threadnum>\n");
     exit(EXIT_FAILURE);
   }
+}
+
+acquire_work()
+{
+  lock(mutex);
+  while(consumer || producer || (strlen(buffer)==0))
+    wait(consumers, mutex);
+  consumers ++;
+  unlock(mutex);
+}
+
+release_work()
+{
+  lock(mutex);
+  consumers--;
+  signal(consumer);
+  signal(producer);
+  unlock(mutex);
+}
+
+(void *) work (void *)
+{
+  acquire_work();
+
+  release_work();
 }
